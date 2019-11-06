ff7aa77111b73f6e2cf6da4d0c9ce750fcebc5ca
xAirx <marcowurtz@hotmail.com>
2019-11-06 17:52:29 +0100

    Updated Swipe Delete and Add Delete on dishDetail

--- ReactNative/confusion/components/DishDetailComponent.js
+++ ReactNative/confusion/components/DishDetailComponent.js
@@ -49,6 +49,30 @@ const mapDispatchToProps = dispatch => ({
   },
 });
 
+const IconFavorite = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+  >
+    <path d="M0 0h24v24H0z" fill="none" />
+    <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
+  </svg>
+);
+
+const IconDelFavorite = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+  >
+    <path d="M0 0h24v24H0z" fill="none" />
+    <path d="M16.5 3c-1.74 0-3.41.81-4.5 2.09C10.91 3.81 9.24 3 7.5 3 4.42 3 2 5.42 2 8.5c0 3.78 3.4 6.86 8.55 11.54L12 21.35l1.45-1.32C18.6 15.36 22 12.28 22 8.5 22 5.42 19.58 3 16.5 3zm-4.4 15.55l-.1.1-.1-.1C7.14 14.24 4 11.39 4 8.5 4 6.5 5.5 5 7.5 5c1.54 0 3.04.99 3.57 2.36h1.87C13.46 5.99 14.96 5 16.5 5c2 0 3.5 1.5 3.5 3.5 0 2.89-3.14 5.74-7.9 10.05z" />
+  </svg>
+);
+
 const formatter = new Intl.DateTimeFormat('en-GB');
 
 function RenderComments(props) {
@@ -197,8 +221,11 @@ class DishDetail extends Component {
     // ////////////////////////// Swipe //////////////////////
     const rightButton = [
       {
+        /* component: IconFavorite, */
         text: 'Add as Favorite',
         type: 'add',
+        scroll: 'no',
+        autoClose: 'yes',
         onPress: () => {
           console.log('WE ARE DELETING SWIPE'), this.addFavorite(dishId);
         },
@@ -207,8 +234,11 @@ class DishDetail extends Component {
 
     const leftButton = [
       {
+        /* component: IconDelFavorite, */
         text: 'Delete Favorite',
         type: 'delete',
+        scroll: 'no',
+        autoClose: 'yes',
         onPress: () => {
           console.log('WE ARE DELETING SWIPE'), this.removeFavorite(dishId);
         },
@@ -224,7 +254,12 @@ class DishDetail extends Component {
     }
     return (
       <ScrollView>
-        <SwipeOut left={leftButton} right={rightButton} autoClose>
+        <SwipeOut
+          left={leftButton}
+          right={rightButton}
+          buttonWidth={150}
+          autoClose
+        >
           <RenderDish
             dish={this.props.dishes.dishes[dishId]}
             // find dish id in favorite to make sure we can show icon
